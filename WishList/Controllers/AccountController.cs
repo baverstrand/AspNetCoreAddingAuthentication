acc560938f759aba35bda5f6541a68db8452ea7f
Sofie Bäverstrand <sofie.baverstrand@consat.se>
2022-09-02 13:55:13 +0200

    authorizw

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using WishList.Models;
 
 namespace WishList.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
